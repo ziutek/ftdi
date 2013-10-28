80082d706fdeed4575f161e5c4cb118226f59779
Michał Derkacz <michal@Lnet.pl>
2013-10-28 13:08:20 +0100

    Add simple utility app: ftdi_out

---
+++ examples/ftdi_out/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ziutek/ftdi"
+	"os"
+	"strconv"
+)
+
+func errorExit(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
+func usageExit() {
+	fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "BYTE")
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		usageExit()
+	}
+	b, err := strconv.ParseUint(os.Args[1], 0, 8)
+	errorExit(err)
+
+	d, err := ftdi.OpenFirst(0x0403, 0x6001, ftdi.ChannelAny)
+	errorExit(err)
+	defer d.Close()
+
+	errorExit(d.SetBitmode(0xff, ftdi.ModeBitbang))
+
+	errorExit(d.WriteByte(byte(b)))
+}
